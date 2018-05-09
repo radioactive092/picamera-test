62cdb466b6bdca76f23ed7e4b04f65a730ddb6c7
radioactive092 <saptarshikundu1995@gmail.com>
2018-05-09 13:23:45 +0530

    v0.6

--- action/dashboard_action.php
+++ action/dashboard_action.php
@@ -3,7 +3,8 @@
 	var_dump($_SERVER['HTTP_HOST']);
 	var_dump($_SERVER['SERVER_ADDR']);
 	$redirect_link = '';
-	$redirect_link = '<a href="'.$_SERVER['SERVER_ADDR'].':7001">View Stream</a>';
+	$redirect_link = '<a href="http://'.$_SERVER['SERVER_ADDR'].':7001">View Stream</a>';
+	$redirect_link_2 = '<a href="http://'.$_SERVER['SERVER_ADDR'].'">View Stream 2</a>';
 	if(isset($_SESSION['username'])) {
 
 	}
